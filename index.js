d6f6f6faeba3864ae3270840357223b402de089b
admin <mumifag_lalolanda@hotmail.com>
2022-06-30 12:35:46 -0400

    updates

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// Mega tuto de electron: https://www.youtube.com/watch?v=0BWzZ6c8z-g
+
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1024,
+    height: 625
   })
 
   win.loadFile('index.html')
